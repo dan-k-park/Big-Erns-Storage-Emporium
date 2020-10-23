ea071e21bdd52a32e1765eaf3d40d76f26502788
danadrol96 <danpark105@gmail.com>
2020-10-23 00:30:23 -0700

    more controller actions

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,15 @@ const controllers = require('../controllers');
 const router = Router();
 
 router.get('/', (req,res) => res.send('This is root!'))
+
 router.post('/admins', controllers.createAdmin)
 router.get('/admins', controllers.getAllAdmins)
 router.get('/admins/:id', controllers.getAdminById)
 router.put('/admins/:id', controllers.updateAdmin)
+
+router.post('/adminlocations', controllers.createAdminLocation)
+router.get('/adminlocations', controllers.getAllAdminLocations)
+router.put('/adminlocations/:id', controllers.updateAdminLocation)
+router.delete('/adminlocations/:id', controllers.deleteAdminLocation)
 
 module.exports = router
