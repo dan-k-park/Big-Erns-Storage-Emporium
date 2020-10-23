0c4acffc22597d28164d928219d0407f6e3353ef
danadrol96 <danpark105@gmail.com>
2020-10-23 00:33:49 -0700

    routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,4 +14,22 @@ router.get('/adminlocations', controllers.getAllAdminLocations)
 router.put('/adminlocations/:id', controllers.updateAdminLocation)
 router.delete('/adminlocations/:id', controllers.deleteAdminLocation)
 
+router.post('/locations', controllers.createLocation)
+router.get('/locations', controllers.getAllLocations)
+router.get('/locations/:id', controllers.getLocationById)
+router.put('/locations/:id', controllers.updateLocation)
+router.delete('/locations/:id', controllers.deleteLocation)
+
+router.post('/units', controllers.createUnit)
+router.get('/units', controllers.getAllUnits)
+router.get('/units/:id', controllers.getUnitById)
+router.put('/units/:id', controllers.updateUnit)
+router.delete('/units/:id', controllers.deleteUnit)
+
+router.post('/users', controllers.createUser)
+router.get('/users', controllers.getAllUsers)
+router.get('/users/:id', controllers.getUserById)
+router.put('/users/:id', controllers.updateUser)
+router.delete('/users/:id', controllers.deleteUser)
+
 module.exports = router
