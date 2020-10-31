df2cd8ff9fb5f728c894ca7dbe0cbabe02ccfad6
danadrol96 <danpark105@gmail.com>
2020-10-31 14:20:47 -0700

    removing dev keys

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  googleClientID: '1009480366047-qjv535efnpmgutkfgqeioq59dml19ic7.apps.googleusercontent.com',
-  googleClientSecret: 'obysmzcd-Bsh4huBTpuqAkkn',
-  cookieKey: 'arygseurigyhseilrhuialhgoiubvb'
-}
