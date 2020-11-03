0afc7d644af977c27f890fe1bce8250c48ef7a2f
danadrol96 <danpark105@gmail.com>
2020-11-02 23:42:44 -0800

    fixed logout

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,16 +4,23 @@ module.exports = (app) => {
   app.get(
     '/auth/google',
     passport.authenticate('google',
-    {
-      scope: ['profile', 'email']
-    })
+      {
+        scope: ['profile', 'email']
+      })
+  );
+  
+  app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/');
+    }
   )
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
-
   app.get('/api/logout', (req, res) => {
+    // logout function is attached automatically to the request object by passport
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   })
 
   app.get('/api/current_user', (req, res) => {
