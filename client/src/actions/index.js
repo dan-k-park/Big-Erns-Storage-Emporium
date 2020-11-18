ed6be90d4bd0f0a725c7ca0b6114a4a3b6e88d8c
danadrol96 <danpark105@gmail.com>
2020-11-17 18:09:36 -0800

    debugging

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,5 +3,8 @@ import { FETCH_USER } from './types'
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
+  console.log(res.data)
+  // this console log shows admin
+  // can we do something to store with this?
   dispatch({ type: FETCH_USER, payload: res.data });
 }
